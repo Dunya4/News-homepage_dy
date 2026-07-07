2a9248498f36b430f93b36efcec1993024c8b565
Dunya4 <dunyaliyeva9@gmail.com>
2026-07-07 23:28:19 +0400

    exit

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const mobile_button = document.querySelector(".nav_mobile_button");
+const nav_mobile = document.querySelector(".nav_mobile");
+const exit_button = document.querySelector(".exit_button");
+
+
+mobile_button.addEventListener("click", function () {
+
+    nav_mobile.classList.add("active");
+    mobile_button.classList.add("hidden");
+})
+
+exit_button.addEventListener("click", function () {
+    mobile_button.classList.remove("hidden");
+    nav_mobile.classList.remove("active");
+})
